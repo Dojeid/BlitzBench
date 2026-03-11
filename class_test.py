9e2e62c72cfa661f5ea294fef54b95caac988052
Saravanan S <104296252+Dojeid@users.noreply.github.com>
2026-03-11 23:09:24 +0530

    init

---
+++ class_test.py
@@ -0,0 +1,10 @@
+print("Hello World")
+class MyClass:
+    def hlo(self):
+        print("Class workin")
+    def loop(self):
+        for i in range(1,12):
+            print(i)
+o=MyClass()
+o.hlo()
+o.loop()
